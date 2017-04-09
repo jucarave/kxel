5711064fc71d5ba68dcce3d98a593271ee9e6a0f
Camilo Ramirez <jucarave92@hotmail.com>
2017-04-08 21:42:49 -0500

    Added watchify to gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var tsify = require('tsify');
 var source = require('vinyl-source-stream');
+var watchify = require('watchify');
 
 gulp.task("bundle", function() {
     return browserify({
@@ -15,6 +16,36 @@ gulp.task("bundle", function() {
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest("dist/js"));
+});
+
+function bundleWatchify(bundle) {
+    console.log("Creating new bundle: " + (new Date()).toISOString());
+
+    bundle.bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest("dist/js"))
+}
+
+gulp.task("watch", function() {
+    var bundle = browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['src/App.ts'],
+        cache: {},
+        packageCache: {}
+    })
+    .plugin(tsify)
+    .plugin(watchify);
+
+    bundle.on('update', function(){
+        bundleWatchify(bundle);
+    });
+
+    bundle.on("log", function(msg) {
+        console.log(msg);
+    });
+
+    bundleWatchify(bundle);
 });
 
 gulp.task("default", ["bundle"]);
